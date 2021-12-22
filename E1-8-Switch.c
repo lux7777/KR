d9956aa2282426dcd9706d3832db6ddd0271b62c
Luka Obelic <luka.obelic@atinel.hr>
2021-12-22 08:59:07 +0100

    E1-8-Switch.c

---
+++ E1-8-Switch.c
@@ -0,0 +1,30 @@
+// count blanks, tabs and newlines with switch statement
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int c, blanks, tabs, nlines;
+
+    blanks = 0;
+    tabs = 0;
+    nlines = 0;
+
+    switch (c = getchar()){
+        case ' ':
+            ++blanks;
+            break;
+        case '\t':
+            ++tabs;
+            break;
+        case '\n':
+            ++nlines;
+            break;
+    }
+
+    printf("blanks = %d\n", blanks);
+    printf("tabs = %d\n", tabs);
+    printf("new lines = %d\n", nlines);
+
+    return 0;
+}
