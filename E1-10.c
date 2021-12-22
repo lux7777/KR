8412cf443ca8c6f764043c4526d5604a25d5fd73
Luka Obelic <luka.obelic@atinel.hr>
2021-12-22 13:41:30 +0100

    E1-10.c

---
+++ E1-10.c
@@ -0,0 +1,25 @@
+/* replace each tab by \t, each backspace by \b and each backslash by \\ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int c;
+
+    while ((c = getchar()) != EOF){
+        if (c == '\t'){
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b'){
+            putchar('\\');
+            putchar('b');            
+        } else if (c == '\\'){
+            putchar('\\');
+            putchar('\\');            
+        } else {
+            putchar(c);
+        }            
+    }    
+
+    return 0;
+}
